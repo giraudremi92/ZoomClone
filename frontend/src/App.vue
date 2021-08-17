7ff7cd7c08e92aa40ede333fea0011820a6b7fe6
giraudremi92 <giraudremi92@gmail.com>
2021-08-17 21:39:55 +0200

    first commit

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,29 @@
 <template>
-<div class="container is-widescreen" style="height:100vh">
+<div class="container is-widescreen" >
 
 
 <div class="columns">
   <div class="column is-two-thirds">
 	<div class="column">
-	<h1 class="title is-1">Company X Visio Room</h1>	
+	<h1 class="title is-1">Visio App</h1>	
 	</div>    
-    <div class="columns is-mobile">
       <div class="column col3">
-	<p>loOn sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'oeil, voire des phrases embarassantes).rem</p>
+<div class="columns is-multiline">
+
+
+  <img  src="https://bulma.io/images/placeholders/128x128.png">
+
+  <img src="https://bulma.io/images/placeholders/128x128.png">
+
+
+  <img  src="https://bulma.io/images/placeholders/128x128.png">
+
+  <img  src="https://bulma.io/images/placeholders/128x128.png">
+
 </div>
-    </div>
+   </div>
   </div>
+
   <div class="column" style="padding: 2rem;">
 	<div class="column has-text-centered col2">
 	<h2 class= " title is-3"><span class="icon">
@@ -43,6 +54,11 @@ export default {
 <style>
 @import "https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.3/css/bulma.min.css";
 @import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css";
+
+html,body {
+    background-color: red;
+}
+
 #app{
 background-color: #21242C;
 color: white;
@@ -65,19 +81,21 @@ padding-top: 2rem;
 margin-top:3rem;
 background-color:#191B1F;
 border-radius: 5px;
-
 }
+
 
 .col4{
 margin-top: 3rem;
 }
 
 
-
 .col-chat{
 max-height:66vh;
 overflow-y: scroll;
 }
-.vdo-container{
+
+img{
+width: 30%;
+padding: 1rem;
 }
 </style>
